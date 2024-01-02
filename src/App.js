78805ac488adff502b2e540a85757d482757af77
EUIJOON <euijoonism@gmail.com>
2024-01-02 13:10:40 +0900

    5. 게임 승자 가리기 - 현재는 유저 입장에서 출력됨.

--- src/App.js
+++ src/App.js
@@ -24,8 +24,9 @@ const choice = {
   }
 }
 function App() {
-  const [userSelect, setUserSelect] = useState(null);
-  const [computerSelect, setComputerSelect] = useState(null);
+  const [userSelect, setUserSelect] = useState(null); // 유저 선택 값 나타내는 것
+  const [computerSelect, setComputerSelect] = useState(null); // 컴퓨터 랜덤 값 나타내는 것
+  const [result, setResult] = useState(null); // 승패 결과 값 나타내는 것
   const play = (userChoice) => {
     // userSelect = choice[userChoice] 이거 안 됨.
     setUserSelect(choice[userChoice])
@@ -33,7 +34,38 @@ function App() {
 
     // 이제 마지막으로 값 집어넣어주기
     setComputerSelect(computerChoice);
+
+    // 판단하는 함수 추가 유저선택값과 컴퓨터랜덤값을 넣어줌.
+    // judgement(choice[userChoice], computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice));
   };
+
+  // 그 새로운 판단함수 만들어주기
+  const judgement = (user, computer) => {
+    console.log("user" , user, "computer", computer);
+    // 경우의 수
+    // 1. user == computer
+    // 2. user rock / computer scissors => user 승
+    // 3. user rock / computer paper => user 패
+    // 4. user scissors / computer paper => user 승
+    // 5. user scissors / computer rock => user 패
+    // 6. user paper / computer rock => user 승
+    // 7. user paper / computer scissors => user 패
+    if(user.name == computer.name) {
+      return "tie";
+    // } else if(user.name == "Rock") {
+    //   return "win";
+    // } else {
+    //   return "lose";
+    // }
+    } else if(user.name == "Rock") 
+      return computer.name=="Scissors" ? "win" : "lose"
+    else if(user.name == "Scissors") 
+      return computer.name=="Paper" ? "win" : "lose"
+    else if(user.name == "Paper") 
+      return computer == "Rock" ? "win" : "lose";
+  };
+
 
   const randomChoice = () => {
     // 배열화시키기위한 작업
@@ -51,8 +83,8 @@ function App() {
   return (
     <div>
       <div className='main'>
-        <Box title = "You" item={userSelect} />
-        <Box title = "Computer" item={computerSelect} />
+        <Box title = "You" item={userSelect} result={result} />
+        <Box title = "Computer" item={computerSelect} result={result} />
       </div>
       <div className='main'>
         <button onClick={() => play("scissors")}>가위</button>
